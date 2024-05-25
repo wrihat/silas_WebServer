366b842459c6e6949dfef93efafcc1f9c3436812
wrihat <479689451@qq.com>
2024-05-24 23:42:48 -0700

    24_5_14

--- include/EventLoop.h
+++ include/EventLoop.h
@@ -2,12 +2,13 @@
 #define EVENTLOOP_H
 class EventLoop{
 public:
-
-
+    EventLoop();
+    ~EventLoop();
 
 private:
-
-
+    void abortNotInLoopThread();
+    bool m_looping;  /*atomic*/
+    const pid_t threadID; 
 }
 
 
